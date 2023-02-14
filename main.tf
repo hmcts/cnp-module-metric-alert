f41fde14d3df6c4835e466d82a7af94535693822
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-14 15:24:09 +0000

    update parameters

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 locals {
-  action_group_rg = "${var.action_group_rg != "" ? var.action_group_rg : var.resourcegroup_name}"
+  action_group_rg = var.action_group_rg != "" ? var.action_group_rg : var.resourcegroup_name
 }
 
 data "template_file" "metricalerttemplate" {
-  template = "${file("${path.module}/templates/Alert.json")}"
+  template = file("${path.module}/templates/Alert.json")
 }
 
 resource "azurerm_resource_group_template_deployment" "custom_alert" {
-  count               = "${var.enabled ? 1 : 0}"
-  template_content       = "${data.template_file.metricalerttemplate.rendered}"
-  name                = "${var.alert_name}"
-  resource_group_name = "${var.resourcegroup_name}"
+  count               = var.enabled ? 1 : 0
+  template_content    = data.template_file.metricalerttemplate.rendered
+  name                = var.alert_name
+  resource_group_name = var.resourcegroup_name
   deployment_mode     = "Incremental"
 
   parameters_content = jsonencode({
@@ -23,7 +23,7 @@ resource "azurerm_resource_group_template_deployment" "custom_alert" {
     frequencyInMinutes       = "${var.frequency_in_minutes}"
     timeWindowInMinutes      = "${var.time_window_in_minutes}"
     severityLevel            = "${var.severity_level}"
-    actionGroupName          = "${var.action_group_name}"
+    actionGroupName          = { value = var.action_group_name }
     customEmailSubject       = "${var.custom_email_subject}"
     appInsightsQuery         = "${var.app_insights_query}"
     actionGroupRg            = "${local.action_group_rg}"
--- variables.tf
+++ variables.tf
@@ -1,43 +1,43 @@
-variable resourcegroup_name {
+variable "resourcegroup_name" {
   description = "The name of the azure resource group"
 }
 
-variable app_insights_name {
+variable "app_insights_name" {
   description = "The name of the application insights resource in Azure"
 }
 
-variable location {
+variable "location" {
   description = "The location of the Azure data center"
 }
 
-variable alert_name {
+variable "alert_name" {
   description = "The name of the alert to create"
 }
 
-variable alert_desc {
+variable "alert_desc" {
   description = "A description of the alert"
 }
 
-variable app_insights_query {
+variable "app_insights_query" {
   description = "The custom query to execute against the logs"
 }
 
-variable frequency_in_minutes {
+variable "frequency_in_minutes" {
   description = "The number of minutes between inspections"
   default     = "5"
 }
 
-variable time_window_in_minutes {
+variable "time_window_in_minutes" {
   description = "The previous window of time over which to execute the query, in minutes"
   default     = "5"
 }
 
-variable severity_level {
+variable "severity_level" {
   description = "The severity of this alert, from 0 to 4 inclusive"
   default     = "3"
 }
 
-variable action_group_name {
+variable "action_group_name" {
   description = "The name of the action group to invoke when the alert is triggered"
 }
 
@@ -46,24 +46,24 @@ variable "action_group_rg" {
   default     = ""
 }
 
-variable custom_email_subject {
+variable "custom_email_subject" {
   description = "The subject of the email sent to the email IDs specified in the action group"
 }
 
-variable trigger_threshold_operator {
+variable "trigger_threshold_operator" {
   description = "The operator to apply to the result with respect to the trigger_threshold. Allowed values are: GreaterThan, Equal and LessThan."
   default     = "GreaterThan"
 }
 
-variable trigger_threshold {
+variable "trigger_threshold" {
   description = "The threshold at which to trigger the alert, with respect to the trigger_threshold_operator"
 }
 
 variable "enabled" {
   description = "Whether alert is created or not"
-  default = "true"
+  default     = "true"
 }
 variable "common_tags" {
   description = "Resource tags"
-  type = map(any)
+  type        = map(any)
 }
