4341b14b2b2c45999098119a5c0a04491afc6584
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-14 14:55:07 +0000

    update parameters

--- main.tf
+++ main.tf
@@ -27,6 +27,6 @@ resource "azurerm_resource_group_template_deployment" "custom_alert" {
     customEmailSubject       = "${var.custom_email_subject}"
     appInsightsQuery         = "${var.app_insights_query}"
     actionGroupRg            = "${local.action_group_rg}"
-    commonTags               = base64encode(var.common_tags)
+    commonTags               = base64encode(jsonencode(var.common_tags))
   })
 }
