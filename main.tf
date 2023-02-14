afb11df00b9664292a423bc6eb256bced24b76df
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-02-14 15:51:43 +0000

    update parameters

--- main.tf
+++ main.tf
@@ -14,19 +14,19 @@ resource "azurerm_resource_group_template_deployment" "custom_alert" {
   deployment_mode     = "Incremental"
 
   parameters_content = jsonencode({
-    alertName                = "${var.alert_name}"
-    alertDesc                = "${var.alert_desc}"
-    appInsightsName          = "${var.app_insights_name}"
-    location                 = "${var.location}"
-    triggerThresholdOperator = "${var.trigger_threshold_operator}"
-    triggerThreshold         = "${var.trigger_threshold}"
-    frequencyInMinutes       = "${var.frequency_in_minutes}"
-    timeWindowInMinutes      = "${var.time_window_in_minutes}"
-    severityLevel            = "${var.severity_level}"
+    alertName                = { value = var.alert_name }
+    alertDesc                = { value = var.alert_desc }
+    appInsightsName          = { value = var.app_insights_name }
+    location                 = { value = var.location }
+    triggerThresholdOperator = { value = var.trigger_threshold_operator }
+    triggerThreshold         = { value = var.trigger_threshold }
+    frequencyInMinutes       = { value = var.frequency_in_minutes }
+    timeWindowInMinutes      = { value = var.time_window_in_minutes }
+    severityLevel            = { value = var.severity_level }
     actionGroupName          = { value = var.action_group_name }
-    customEmailSubject       = "${var.custom_email_subject}"
-    appInsightsQuery         = "${var.app_insights_query}"
-    actionGroupRg            = "${local.action_group_rg}"
+    customEmailSubject       = { value = var.custom_email_subject }
+    appInsightsQuery         = { value = var.app_insights_query }
+    actionGroupRg            = { value = local.action_group_rg }
     commonTags               = base64encode(jsonencode(var.common_tags))
   })
 }
